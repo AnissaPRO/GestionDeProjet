0d250c6915e1560f17b1a8e0cf4affe40b0e3546
AnissaPRO <anissa.elbouazzaoui@gmail.com>
2021-11-26 10:39:44 +0100

    sprint 1

---
+++ EspaceAdministrateur.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Espace Administration</title>
+</head>
+<body>
+<p>     Controller Connexion Deconnexion
+        Vue formulaire de Connexion
+        Création de la base de données
+        Controller Espace Administration
+        Vue Espace Conseiller
+        Déploiement</p> 
+</body>
+</html>
