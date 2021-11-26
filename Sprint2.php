7245d05c3887834b239b4b45839b5ea67d35b182
AnissaPRO <anissa.elbouazzaoui@gmail.com>
2021-11-26 10:53:54 +0100

    deuxième sprint

---
+++ Sprint2.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sprint 2</title>
+</head>
+<body>
+    <h1>En tant que vendeur, je veux consulter les propositions reçus pour mon bien afin de les étudier</h1>
+    <p> Controller Connexion Deconnexion
+        Vue formulaire de Connexion
+        Création de la base de données
+        Controller biens à la vente 
+        Vue bien à la vente
+        Controller messagerie/proposition
+        Vue messagerie/proposition
+        Création du formulaire soumettre un bien a vendeur
+        Création de la table bien
+        Controller Espace Vendeur
+        Vue Espace Vendeur
+        Déploiement</p>
+
+
+
+        <h1>En tant que conseiller, je veux visualiser les messages envoyés par les acheteurs afin de répondre à leurs questions</h1>
+    <p> Controller Connexion Deconnexion
+        Vue formulaire de Connexion
+        Création de la base de données
+        Controller messagerie/Questions
+        Vue messagerie/Question
+        Controller Espace Conseiler
+        Vue Espace Conseiler
+        Déploiement</p>
+
+    
+</body>
+</html>
